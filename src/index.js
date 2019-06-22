199ff5ed53bf7857f7e42a592b4c6dafa62b7aa4
Harry Chung <hc9825@gmail.com>
2019-06-22 13:23:43 -0700

    context provider change

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './paltrows-power-toes/App';
+// import { BrowserRouter } from 'react-router-dom';
+import AppLang from './lang-context/AppLang';
 import './index.css';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>, 
+    <AppLang/>, 
     document.getElementById('root')
 );
